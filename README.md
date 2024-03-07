9e5b7b0193970d014a371cba92417b6392bdaf2d
lhy <529568252@qq.com>
2024-03-08 00:12:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Windows-Wireguard-Watchdog
+
+> 背景：Wireguard服务端是动态IP，Wireguard客户端配置的服务端地址是域名 Endpoint = xx.xxx.com:51820，写的DDNS脚本检测到IP变更会自动更新域名解析。每次宽带重拨IP就会变更，由于客户端重连使用的不是域名而是第一次连接时解析的域名对应的IP地址，导致一直连接不上服务端。
+
+使用此PowerShell脚本，可以让你的Wireguard保持稳定的连接，服务端ip变更客户端仍可自动重新连接。
+
+## 脚本流程
+
+脚本主要执行了一下的步骤:
+
+1. 解析配置文件读取连接服务端的域名。
+2. 查询DNS服务器，获取A记录的IP地址。
+3. 循环获取查询DNS，如果IP变更，把原配置文件的Endpoint替换成新的IP，生成新的配置文件，然后重新启动Wireguard服务。
+4. 如果查询DNS服务器失3次，就停止Wireguard服务（防止因为Wireguard连接不上服务端导致查询DNS失败），然后再次执行第3步。
+
+为什么要用新的IP生成配置文件？
+
+Wireguard不会用我们配置的DNS获取IP，我们通过配置的DNS检测到IP变更，但是Wireguard解析域名可能检测不到变化，试过清除本地DNSClient缓存，不起作用，重启Wireguard服务他还是用原来的IP，导致连接失败。
+
+## 安装步骤
+
+让脚本以Windows服务方式运行 :
+
+1. 修改脚本中的Wireguard 以下几项配置
+	```powershell
+	# wireguard 配置文件
+	$WireguardConfigFilePath = "C:\Users\admin\Desktop\company.conf" 
+	# 检查IP变更时间间隔，秒
+	$IntervalSeconds = 10
+	# DNS服务器地址,不设置会从 wireguard 配置文件中读取DNS参数。我的域名是阿里的，39.96.153.52是dns9.hichina.com的ip，能立即检测到域名解析的ip变更
+	$DNS = "39.96.153.52" 
+	```
+	
+2. 用powershell终端管理员执行
+	```shell
+	.\nssm.exe install MyWireGuardService "powershell.exe" "-ExecutionPolicy Bypass -File C:\Users\admin\Desktop\keep_wireguard_alive.ps1"
+	```
+	替换 `C:\Users\admin\Desktop\keep_wireguard_alive.ps1` 为你自己的脚本文件路径。
+	
+3. 启动服务
+	```shell
+	.\nssm.exe start MyWireGuardService
+	```
+   现在脚本就作为Windows服务运行了，你可以打开Wireguard的UI查看日志，任务管理器查看Wireguard的进程。
+   
+## 关闭脚本服务
+
+停止服务
+
+```shell
+.\nssm.exe stop MyWireGuardService
+```
+
+移除服务
+
+```shell
+.\nssm.exe remove MyWireGuardService
+```
+
+## 停止Wireguard服务
+
+```shell
+wireguard /uninstalltunnelservice companytemp
+```
+
+替换`company`为你的Wireguard 配置文件名（company.conf不包含扩展名），或者任务管理器停止服务
+
+## Wireguard客户端配置例子
+
+```
+
+[Interface]
+PrivateKey = WCCQic09ih+u/Xr1v4FXDtXKpzgx9JLr59hRo2hX3E=
+Address = 10.0.8.3/24
+DNS = 114.114.114.114
+
+
+[Peer]
+PublicKey = H1QI7lFziAopeQfMm61ZMSNvxJeus1KGXZMKw/uoCY=
+PresharedKey = l/B0jMWfDqpYD2UrK1H0uNlv4/qpthtPtLvt/QIDU8=
+AllowedIPs = 0.0.0.0/0,::/0
+PersistentKeepalive = 25
+Endpoint = xxx.yyy.com:51820
+```
+
+## 创作不易，欢迎打赏
+
+<img src="http://home.dcssn.com:81/markdown/wechat-payment-code.jpg" style="zoom:30%;" />
+<img src="http://home.dcssn.com:81/markdown/ali-payment-code.jpg" style="zoom:20%;" />
+
+1分钱就可以
---
+++ keep_wireguard_alive.ps1
@@ -0,0 +1,140 @@
+# wireguard 配置文件
+$WireguardConfigFilePath = "C:\Users\admin\Desktop\company.conf" 
+# 检查IP变更时间间隔
+$IntervalSeconds = 10
+# DNS服务器地址,不设置会从 wireguard 配置文件中读取
+$DNS = "39.96.153.52"
+
+# 通过文件路径获取文件名（不包含扩展名）
+function getFilenameByPath {
+    param (
+        [string]$path
+    )
+    $tempArr = $path.Split("\")
+    return $tempArr[$tempArr.Length - 1].Split(".")[0] + "temp"
+}
+
+# 读取配置文件获取服务端连接域名
+function getEndpointByFile {
+    param (
+        [string]$path
+    )
+    Get-Content -Path $path | ForEach-Object { 
+        if ($_ -like "*Endpoint*") {
+            return $_.Split('=')[1].Split(':')[0].Trim() 
+        }
+    }
+}
+
+# 读取配置文件获取DNS服务器地址
+function getDNSByFile {
+    param (
+        [string]$path
+    )
+    if ($DNS) {
+        return $DNS
+    }
+    Get-Content -Path $path | ForEach-Object { 
+        if ($_ -like "*DNS*") {
+            return $_.Split('=')[1].Trim()
+        }
+    }
+}
+
+# 获取dns解析的ip
+function getDnsIp() {
+    param (
+        [string]$domain,
+        [string]$DnsServer
+    )
+    # 调用 Resolve-DnsName 命令并指定参数 -Type A（A记录）
+    $result = Resolve-DnsName $domain -Server $DnsServer -Type A -DnsOnly -QuickTimeout
+    $ip = "failed"
+    if ($result) {
+        $ip = $result[0].IPAddress
+    }
+    else {
+        $result = Resolve-DnsName $domain -Server "114.114.114.114" -Type A -DnsOnly -QuickTimeout
+        if ($result) {
+            $ip = $result[0].IPAddress
+        }
+    }
+    return $ip
+}
+
+# 初始化隧道，服务方式运行wireguard
+function Start-WireguardTunnel {
+    param (
+        [string]$TunnelName,
+        [string]$EndpointIp
+    )
+    $serviceName = "WireGuard Tunnel: $TunnelName";
+    $ExistsService = Get-Service $serviceName;
+    if ($ExistsService) {
+        Write-Host "stop wireguard"
+        Invoke-Expression "wireguard /uninstalltunnelservice $TunnelName"
+    }
+    Write-Host "start wireguard"
+    Start-Sleep -Seconds 3
+    # 生成临时配置文件
+    $WireguardConfigFilePathTemp = $WireguardConfigFilePath.Substring(0, $WireguardConfigFilePath.Length - $TunnelName.Length - 1) + "\" + "$TunnelName" + ".conf"
+    if (-not(Test-Path $WireguardConfigFilePathTemp -PathType Leaf)) {
+        New-Item -ItemType File -Path $WireguardConfigFilePathTemp 
+        Set-ItemProperty -Path $WireguardConfigFilePathTemp -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+    }
+    $Endpoint = getEndpointByFile -path $WireguardConfigFilePath
+    $Content = Get-Content -Path $WireguardConfigFilePath
+    $NewContent = $Content.Replace($Endpoint, $EndpointIp)
+    Set-Content -Path $WireguardConfigFilePathTemp -Value $NewContent
+    Invoke-Expression "wireguard /installtunnelservice $WireguardConfigFilePathTemp"
+}
+
+# 停止WireGuard服务
+function Stop-WireGuardServic {
+    param (
+        [string]$TunnelName
+    )
+    Write-Host "stop wireguard"
+    Invoke-Expression "wireguard /uninstalltunnelservice $TunnelName"
+}
+# 隧道名称
+$TunnelName = getFilenameByPath -path $WireguardConfigFilePath
+# 服务端连接域名
+$Endpoint = getEndpointByFile -path $WireguardConfigFilePath
+# DNS服务器地址
+$DnsServer = getDNSByFile -path $WireguardConfigFilePath
+# wireguard 解析的ip
+$EndpointIPAddress = getDnsIp -domain $Endpoint -DnsServer $DnsServer
+# dns 解析的ip
+$DnsIPAddress = $EndpointIPAddress
+# getDnsIp 失败次数
+$DnsFailedCount = 0
+# 初始化隧道
+Start-WireguardTunnel -TunnelName $TunnelName -EndpointIp $DnsIPAddress
+# 主程序
+while ($true) {
+    $DnsIPAddress = getDnsIp -domain $Endpoint -DnsServer $DnsServer
+    Write-Host "DnsIPAddress: $DnsIPAddress, EndpointIPAddress: $EndpointIPAddress, DnsServer: $DnsServer"
+    if ($DnsIPAddress -eq "failed") {
+        $DnsFailedCount = $DnsFailedCount + 1
+        if ($DnsFailedCount -eq 3) {
+            Stop-WireGuardServic -TunnelName $TunnelName
+            $DnsFailedCount = 1
+        }
+    }
+    else {
+        $DnsFailedCount = 1
+        if ($EndpointIPAddress -ne $DnsIPAddress) {
+            Start-WireguardTunnel -TunnelName $TunnelName -EndpointIp $DnsIPAddress
+            $EndpointIPAddress = $DnsIPAddress
+        }
+        else {
+            $serviceName = "WireGuard Tunnel: $TunnelName";
+            $ExistsService = Get-Service $serviceName;
+            if (!$ExistsService) {
+                Start-WireguardTunnel -TunnelName $TunnelName -EndpointIp $DnsIPAddress
+            }
+        }
+    }
+    Start-Sleep -Seconds $IntervalSeconds
+}
